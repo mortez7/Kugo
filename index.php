e9656f5d6704b8a07958d3084758c552f153e711
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-12-08 11:22:20 +0200

    fix: html errors

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/style.css">
+  <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./img/favicon.ico" type="image/x-icon">
   <script>/*! modernizr 3.6.0 (Custom Build) | MIT *
     *https://modernizr.com/download/?-setclasses !*/
       !function(n,e,s){function o(n,e){return typeof n===e}function a(){var n,e,s,a,i,l,r;for(var c in f)if(f.hasOwnProperty(c)){if(n=[],e=f[c],e.name&&(n.push(e.name.toLowerCase()),e.options&&e.options.aliases&&e.options.aliases.length))for(s=0;s<e.options.aliases.length;s++)n.push(e.options.aliases[s].toLowerCase());for(a=o(e.fn,"function")?e.fn():e.fn,i=0;i<n.length;i++)l=n[i],r=l.split("."),1===r.length?Modernizr[r[0]]=a:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=a),t.push((a?"":"no-")+r.join("-"))}}function i(n){var e=r.className,s=Modernizr._config.classPrefix||"";if(c&&(e=e.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+s+"no-js(\\s|$)");e=e.replace(o,"$1"+s+"js$2")}Modernizr._config.enableClasses&&(e+=" "+s+n.join(" "+s),c?r.className.baseVal=e:r.className=e)}var t=[],f=[],l={_version:"3.6.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(n,e){var s=this;setTimeout(function(){e(s[n])},0)},addTest:function(n,e,s){f.push({name:n,fn:e,options:s})},addAsyncTest:function(n){f.push({name:null,fn:n})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var r=e.documentElement,c="svg"===r.nodeName.toLowerCase();a(),i(t),delete l.addTest,delete l.addAsyncTest;for(var u=0;u<Modernizr._q.length;u++)Modernizr._q[u]();n.Modernizr=Modernizr}(window,document);
   </script>
-  <title>Kugo</title>
+  <title>Kugo - Главная</title>
 </head>
 <body>
   <div class="container">
@@ -54,14 +56,14 @@
     <header class="header">
       <a href="./" class="header-logo">Kugoo</a>
 
-      <button class="button header-button">
-        <div class="header-button-lines">
-          <div class="header-button-line"></div>
-          <div class="header-button-line"></div>
-          <div class="header-button-line"></div>
+      <a href="#" class="button header-button">
+        <div class="lines">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
         </div>
         Каталог
-      </button>
+      </a>
 
       <div class="header-controls">
         <input type="text" class="header-input" placeholder="Искать самокат KUGO">
@@ -589,12 +591,12 @@
           <ul class="footer-contacts-list footer-menu-column-2">
             <li class="footer-contacts-item">
               <p class="footer-contacts-title">Call-центр</p>
-              <a href="#" type="tel:+78005055461" class="footer-contacts-phone">+7 (800) 505-54-61</a>
+              <a href="tel:+78005055461" class="footer-contacts-phone">+7 (800) 505-54-61</a>
               <p class="footer-contacts-schedule">Пн-Вс 10:00 - 20:00</p>
             </li>
             <li class="footer-contacts-item">
               <p class="footer-contacts-title">Сервисный центр</p>
-              <a href="#" type="tel:+74993507692" class="footer-contacts-phone">+7 (499) 350-76-92</a>
+              <a href="tel:+74993507692" class="footer-contacts-phone">+7 (499) 350-76-92</a>
               <p class="footer-contacts-schedule">Пн-Вс 10:00 - 20:00</p>
             </li>
           </ul>
